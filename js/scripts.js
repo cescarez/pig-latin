c221f6083284c28a77cefb58f389e005c4db07d1
Christabel Escarez <christabel.escarez@gmail.com>
2018-10-07 09:50:10 -0700

    add way to the end of words with vowel as first letter and move first consonant + ay -- doesn't work with multiple consonants in a row

--- js/scripts.js
+++ js/scripts.js
@@ -20,21 +20,21 @@ $(document).ready(function(){
       console.log(inputArray);
 
       for (i = 0; i < inputArray.length; i++) {
-      inputCount += 1;
+        inputCount += 1;
       var currentChar = inputArray[i];
       console.log(currentChar);
       console.log(inputCount);
-        if (inputCount === 1 && currentChar === "a" || inputCount === 1 && currentChar === "A" || inputCount === 1 && currentChar === "e" || inputCount === 1 && currentChar === "E" ||  inputCount === 1 && currentChar === "i" || inputCount === 1 && currentChar === "I" ||  inputCount === 1 && currentChar === "o" || inputCount === 1 && currentChar === "O" ||  inputCount === 1 && currentChar === "u" || inputCount === 1 && currentChar === "U") {
+        if (inputCount === 1 && inputArray[0] === "a" || inputCount === 1 && inputArray[0] === "A" || inputCount === 1 && inputArray[0] === "e" || inputCount === 1 && inputArray[0] === "E" || inputCount === 1 && inputArray[0] === "i" || inputCount === 1 && inputArray[0] === "I" || inputCount === 1 && inputArray[0] === "o" || inputCount === 1 && inputArray[0] === "O" || inputCount === 1 && inputArray[0] === "u" || inputCount === 1 && inputArray[0] === "U") {
           $("#output").text(inputArray.join("") + "way")
           console.log("vowel first letter");
           console.log(inputCount);
           break;
-        } else if (inputCount > 1 && currentChar === "a" || inputCount > 1 && currentChar === "A" || inputCount > 1 && currentChar === "e" || inputCount > 1 && currentChar === "E" || inputCount > 1 && currentChar === "i" || inputCount > 1 && currentChar === "I" || inputCount > 1 && currentChar === "o" || inputCount > 1 && currentChar === "O" || inputCount > 1 && currentChar === "u" || inputCount > 1 && currentChar === "U") {
+        } else if (inputCount > 1 && inputArray[0] === "a" || inputCount > 1 && inputArray[0] === "A" || inputCount > 1 && inputArray[0] === "e" || inputCount > 1 && inputArray[0] === "E" || inputCount > 1 && inputArray[0] === "i" || inputCount > 1 && inputArray[0] === "I" || inputCount > 1 && inputArray[0] === "o" || inputCount > 1 && inputArray[0] === "O" || inputCount > 1 && inputArray[0] === "u" || inputCount > 1 && inputArray[0] === "U") {
           $("#output").text(inputArray.join("") + concatArray.join("") + "ay");
           console.log("vowel later");
           break;
-        } else {
-          concatArray.push(currentChar);
+        } else if (inputArray[0] !== "a" || inputArray[0] !== "A" || inputArray[0] !== "e" || inputArray[0] !== "E" || inputArray[0] !== "i" || inputArray[0] !== "I" || inputArray[0] !== "o" || inputArray[0] !== "O" || inputArray[0] !== "u" || inputArray[0] !== "U") {
+          concatArray.push(inputArray[0]);
           inputArray.splice(0, 1);
           console.log(inputArray);
           continue;
