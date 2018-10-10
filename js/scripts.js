45db34eff18c34ebdfac26e602c206bc96a17dd4
Christabel Escarez <christabel.escarez@gmail.com>
2018-10-09 20:01:04 -0700

    separate business and UX logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,26 @@ var inputArray = [];
 var inputCount = 0;
 var concatArray = [];
 
+var checkVowel = function(word) {
+  for (i = 0; i < word.length; i) {
+    inputCount += 1;
+    if (inputCount === 1 && inputArray[0] === "a" || inputCount === 1 && inputArray[0] === "A" || inputCount === 1 && inputArray[0] === "e" || inputCount === 1 && inputArray[0] === "E" || inputCount === 1 && inputArray[0] === "i" || inputCount === 1 && inputArray[0] === "I" || inputCount === 1 && inputArray[0] === "o" || inputCount === 1 && inputArray[0] === "O" || inputCount === 1 && inputArray[0] === "u" || inputCount === 1 && inputArray[0] === "U") {
+      $("#output").text(inputArray.join("") + "way")
+      console.log("vowel first letter");
+      break;
+    } else if (inputCount > 1 && inputArray[0] === "a" || inputCount > 1 && inputArray[0] === "A" || inputCount > 1 && inputArray[0] === "e" || inputCount > 1 && inputArray[0] === "E" || inputCount > 1 && inputArray[0] === "i" || inputCount > 1 && inputArray[0] === "I" || inputCount > 1 && inputArray[0] === "o" || inputCount > 1 && inputArray[0] === "O" || inputCount > 1 && inputArray[0] === "u" || inputCount > 1 && inputArray[0] === "U") {
+      $("#output").text(inputArray.join("") + concatArray.join("") + "ay");
+      console.log("vowel later");
+      break;
+    } else if (inputArray[0] !== "a" || inputArray[0] !== "A" || inputArray[0] !== "e" || inputArray[0] !== "E" || inputArray[0] !== "i" || inputArray[0] !== "I" || inputArray[0] !== "o" || inputArray[0] !== "O" || inputArray[0] !== "u" || inputArray[0] !== "U") {
+      concatArray.push(inputArray[0]);
+      inputArray.splice(0, 1);
+      console.log(inputArray);
+      continue;
+    }
+  }
+}
+
 $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
@@ -17,29 +37,7 @@ $(document).ready(function(){
       for (i = 0 ; i < inputString.length; i++) {
         inputArray.push(inputString[i]);
       }
-      console.log(inputArray);
-
-      for (i = 0; i < inputArray.length; i) {
-        inputCount += 1;
-      var currentChar = inputArray[i];
-      console.log(currentChar);
-      console.log(inputCount);
-        if (inputCount === 1 && inputArray[0] === "a" || inputCount === 1 && inputArray[0] === "A" || inputCount === 1 && inputArray[0] === "e" || inputCount === 1 && inputArray[0] === "E" || inputCount === 1 && inputArray[0] === "i" || inputCount === 1 && inputArray[0] === "I" || inputCount === 1 && inputArray[0] === "o" || inputCount === 1 && inputArray[0] === "O" || inputCount === 1 && inputArray[0] === "u" || inputCount === 1 && inputArray[0] === "U") {
-          $("#output").text(inputArray.join("") + "way")
-          console.log("vowel first letter");
-          console.log(inputCount);
-          break;
-        } else if (inputCount > 1 && inputArray[0] === "a" || inputCount > 1 && inputArray[0] === "A" || inputCount > 1 && inputArray[0] === "e" || inputCount > 1 && inputArray[0] === "E" || inputCount > 1 && inputArray[0] === "i" || inputCount > 1 && inputArray[0] === "I" || inputCount > 1 && inputArray[0] === "o" || inputCount > 1 && inputArray[0] === "O" || inputCount > 1 && inputArray[0] === "u" || inputCount > 1 && inputArray[0] === "U") {
-          $("#output").text(inputArray.join("") + concatArray.join("") + "ay");
-          console.log("vowel later");
-          break;
-        } else if (inputArray[0] !== "a" || inputArray[0] !== "A" || inputArray[0] !== "e" || inputArray[0] !== "E" || inputArray[0] !== "i" || inputArray[0] !== "I" || inputArray[0] !== "o" || inputArray[0] !== "O" || inputArray[0] !== "u" || inputArray[0] !== "U") {
-          concatArray.push(inputArray[0]);
-          inputArray.splice(0, 1);
-          console.log(inputArray);
-          continue;
-        }
-      }
+      checkVowel(inputString);
     }
   });
 });
